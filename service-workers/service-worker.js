84932d49bb951cf059acee07a708e04e77828a58
Sanjay Maurya <sanjay.m@ebrandz.com>
2017-06-15 11:02:05 +0530

    Initial commit

--- service-workers/service-worker.js
+++ service-workers/service-worker.js
@@ -1,8 +1,8 @@
 
 self.addEventListener('install', function(e) {
-	console.log('Install Event: ' + e);
+	console.log('Install Event: ', e);
 });
 
 self.addEventListener('activate', function(e) {
-	console.log('Activate Event: ' + e);
+	console.log('Activate Event: ', e);
 });
